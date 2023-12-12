5f4f11722fd6f47f77667c574860eb76fff81028
AlenaFelicita <118136714+AlenaFelicita@users.noreply.github.com>
2023-12-12 15:34:35 +0300

    Create Task_1.py

---
+++ Task_1.py
@@ -0,0 +1,35 @@
+import requests
+import yaml
+import pytest
+
+with open('config.yaml') as file:
+    my_dict=yaml.safe_load(file)
+
+url = my_dict['url']
+url1 = my_dict['url1']
+website = 'https://test-stand.gb.ru/api/posts'
+username = 'Alena808A'
+password = 'gi89mjb90f'
+
+
+
+#
+# def login(username, password):
+#     obj_data = requests.post(url=url, data={'username':username, 'password': password})
+#     token = obj_data.json()['token']
+#     print(token)
+#     return token
+
+
+def token_auth(token):
+    res = requests.get(url=my_dict["url1"], headers={"X-Auth-Token": token}, params={"owner": "notMe"})
+    content_var = [item["content"] for item in res.json()['data']]
+    return content_var
+
+def test_step1(login):
+    assert '' in token_auth(login)
+
+def test_step2(postP):
+    assert 'Anything' in postP
+
+print(token_auth(my_dict['token']))
