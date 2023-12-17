3d75c86861047316af18cfcdf2fcd4386b3b6751
AlenaFelicita <118136714+AlenaFelicita@users.noreply.github.com>
2023-12-17 11:53:04 +0300

    Create Task2_module.py

---
+++ Task2_module.py
@@ -0,0 +1,43 @@
+import yaml
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
+with open("testdata.yaml") as f:
+    testdata = yaml.safe_load(f)
+    browser = testdata["browser"]
+
+
+class Site:
+    def __init__(self, address):
+        if browser == "firefox":
+            service = Service(executable_path=GeckoDriverManager().install())
+            options = webdriver.FirefoxOptions()
+            self.driver = webdriver.Firefox(service=service, options=options)
+        elif browser == "chrome":
+            service = Service(executable_path=ChromeDriverManager().install())
+            options = webdriver.ChromeOptions()
+            self.driver = webdriver.Chrome(service=service, options=options)
+        self.driver.implicitly_wait(3)
+        self.driver.maximize_window()
+        self.driver.get(address)
+        time.sleep(testdata["sleep_time"])
+
+    def find_element(self, mode, path):
+        if mode == "css":
+            element = self.driver.find_element(By.CSS_SELECTOR, path)
+        elif mode == "xpath":
+            element = self.driver.find_element(By.XPATH, path)
+        else:
+            element = None
+        return element
+
+    def get_element_property(self, mode, path, property):
+        element = self.find_element(mode, path)
+        return element.value_of_css_property(property)
+
+    def close(self):
+        self.driver.close()
